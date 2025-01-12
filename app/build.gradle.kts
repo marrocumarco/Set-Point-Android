6410067f361aa04504652f490757ba1a9c1cecf4
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-01-12 15:19:10 +0100

    target sdk changed to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.marrocumarcodeveloper.set_point"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
