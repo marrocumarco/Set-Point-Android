e14866b9cac8d9bdeeb8bc5941478fdf11cc0a95
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-01-19 19:23:33 +0100

    Refactor: removed useless point value

--- app/src/main/kotlin/com/marrocumarcodeveloper/set_point/business_logic/Point.kt
+++ app/src/main/kotlin/com/marrocumarcodeveloper/set_point/business_logic/Point.kt
@@ -1,5 +1,5 @@
 package com.marrocumarcodeveloper.set_point.business_logic
 
 enum class Point(val value: Int) {
-    ZERO(0), FIFTEEN(1), THIRTY(2), FORTY(3), ADVANTAGE(4), GAME_WON(5)
+    ZERO(0), FIFTEEN(1), THIRTY(2), FORTY(3), ADVANTAGE(4)
 }
