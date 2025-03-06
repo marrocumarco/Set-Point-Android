75dd82a4105ba28a8dcad66f928006189f9b4a44
Marco Marrocu <marrocumarcozaggi@gmail.com>
2025-03-06 20:54:49 +0100

    Updated version code and reduced target sdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,9 +32,9 @@ android {
     defaultConfig {
         applicationId = "com.marrocumarcodeveloper.set_point"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 34
+        versionCode = 3
+        versionName = "1.1.0"
         vectorDrawables {
             useSupportLibrary = true
         }
